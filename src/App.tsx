3a538836af618b62b3f66a84b51f4361e66e4c9b
Nicholas Ng <onenylxus@gmail.com>
2025-11-07 01:31:37 +0800

    Setup React environment for component development

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import './App.css';
+
+function App() {
+  return (
+    <div
+      className="container"
+      style={{
+        width: '100vw',
+        height: '100vh',
+        background: '#f8f8f8',
+        margin: 0,
+        padding: 0,
+      }}
+    ></div>
+  );
+}
+
+export default App;
